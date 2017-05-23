1c3db5a29faa8749b8366d748c8129016cbf93e9
Vishal Dedania <vdedaniya@gmail.com>
2017-05-23 17:28:54 +0000

    Adding generic repository for CRUD operations on each db model objects

---
+++ MRF/MRF.Data/GenericRepository.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+
+namespace MRF.Data
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        protected DbSet<T> DbSet { get; set; }
+        protected DbContext Context { get; set; }
+
+        public GenericRepository(DbContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentException("An instance of DbContext is " +
+                    "required to use this repository.", "context");
+            }
+
+            this.Context = context;
+            this.DbSet = this.Context.Set<T>();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return this.DbSet;
+        }
+
+        public T GetById(long id)
+        {
+            return this.DbSet.Find(id);
+        }
+
+        public void Add(T entity)
+        {
+            DbEntityEntry entry = this.Context.Entry(entity);
+            if (entry.State != EntityState.Detached)
+            {
+                entry.State = EntityState.Added;
+            }
+            else
+            {
+                this.DbSet.Add(entity);
+            }
+        }
+
+        public void Update(T entity)
+        {
+            DbEntityEntry entry = this.Context.Entry(entity);
+
+            if (entry.State == EntityState.Detached)
+            {
+                this.DbSet.Attach(entity);
+            }
+
+            entry.State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            DbEntityEntry entry = this.Context.Entry(entity);
+
+            entry.State = EntityState.Detached;
+        }
+
+        public void Delete(int id)
+        {
+            var entity = this.GetById(id);
+
+            if (entity != null)
+            {
+                this.Delete(entity);
+            }
+        }
+
+        public void Detach(T entity)
+        {
+            DbEntityEntry entry = this.Context.Entry(entity);
+
+            if (entry.State != EntityState.Deleted)
+            {
+                entry.State = EntityState.Deleted;
+            }
+            else
+            {
+                this.DbSet.Attach(entity);
+                this.DbSet.Remove(entity);
+            }
+        }
+    }
+}
---
+++ MRF/MRF.Data/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace MRF.Data
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        T GetById(long id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Delete(int id);
+        void Detach(T entity);
+    }
+}

7730b60c2d7adb0db882d59b38080a82de46219b
Vishal Dedania <vdedaniya@gmail.com>
2017-05-23 17:01:23 +0000

    Adding models for taxi rates and state

---
+++ MRF/MRF.Models/State.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRF.Models
+{
+    public class State
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public string StateName { get; set; }
+
+        [Required]
+        public string StateId { get; set; }
+    }
+}
---
+++ MRF/MRF.Models/TaxiRate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MRF.Models
+{
+    public class TaxiRate : IAuditInfo
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        public decimal UnitReate { get; set; }
+
+        [Required]
+        public decimal InitialEntryFee { get; set; }
+
+        [Required]
+        public decimal TaxSurcharge { get; set; }
+
+        [Required]
+        public State StateId { get; set; }
+
+        [Required]
+        public decimal PeakHoursSurcharge { get; set; }
+
+        [Required]
+        public decimal NightTimeSurcharge { get; set; }
+
+        [Required]
+        public decimal TimedUnitsPerMinute { get; set; }
+
+        [Required]
+        public decimal DistanceUnitsPerMile { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}

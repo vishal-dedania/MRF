c203bc5b9957cff0094788275c62cb547e0f8bdf
Vishal Dedania <vdedaniya@gmail.com>
2017-05-23 17:19:12 +0000

    Adding EntityTypeConfiguration for TaxiRate model

--- MRF/MRF.Data/Configuration/StateConfiguration.cs
+++ MRF/MRF.Data/Configuration/StateConfiguration.cs
@@ -1,10 +1,13 @@
 ﻿using System.Data.Entity.ModelConfiguration;
+using System.Threading;
 using MRF.Models;
 
 namespace MRF.Data.Configuration
 {
     public class StateConfiguration : EntityTypeConfiguration<State>
     {
-        
+        public StateConfiguration()
+        {
+        }
     }
 }
--- MRF/MRF.Data/Configuration/TaxiRateConfiguration.cs
+++ MRF/MRF.Data/Configuration/TaxiRateConfiguration.cs
@@ -5,6 +5,13 @@ namespace MRF.Data.Configuration
 {
     public class TaxiRateConfiguration : EntityTypeConfiguration<TaxiRate>
     {
-        
+        public TaxiRateConfiguration()
+        {
+            Property(p => p.CreatedOn)
+                .IsRequired().HasColumnType("datetime");
+
+            Property(p => p.UpdatedOn)
+                .IsRequired().HasColumnType("datetime");
+        }
     }
 }
